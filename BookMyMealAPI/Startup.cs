e99eef5f61cc537160db75de8b9a3db36433b4e8
Sajidur Rahman <msrkms@hotmail.com>
2020-02-02 18:16:25 +0600

    Swagger Middleware Added

--- BookMyMealAPI/Startup.cs
+++ BookMyMealAPI/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace BookMyMealAPI
 {
@@ -25,6 +26,11 @@ namespace BookMyMealAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookMyMeal API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,9 +41,20 @@ namespace BookMyMealAPI
                 app.UseDeveloperExceptionPage();
             }
 
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookMyMeal API");
+                c.RoutePrefix = string.Empty;
+            });
             app.UseRouting();
 
             app.UseAuthorization();
+
+
+
 
             app.UseEndpoints(endpoints =>
             {

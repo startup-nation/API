e33da9d29876eda72a48a0f21380f56473149900
Sajidur Rahman <msrkms@hotmail.com>
2020-03-11 20:12:40 +0600

    Profile Update

--- BookMyMealAPI/Controllers/ProfileController.cs
+++ BookMyMealAPI/Controllers/ProfileController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using BookMyMealAPI.Data;
 using BookMyMealAPI.Model.Entity;
+using BookMyMealAPI.Model.Request;
 using BookMyMealAPI.Model.Response;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookMyMealAPI.Controllers
 {
@@ -52,7 +54,46 @@ namespace BookMyMealAPI.Controllers
         }
 
 
+        [HttpPost]
+        [Route("Update")]
+        [Authorize(Roles = "Customer")]
+        public async Task<Object> UpdatePofile(ProfileUpdateRequestModel updateRequestModel)
+        {
+            ProfileUpdateResponseModel responseModel = new ProfileUpdateResponseModel();
+            string userId = User.Claims.First(c => c.Type == "UserID").Value;
+            var user = await _userManager.FindByIdAsync(userId);
+            ProfileModel profile = await _context.Profile.FindAsync(user.Email);
+            if (profile == null)
+            {
+                return NotFound();
+            }
 
-        
+            profile.Name = updateRequestModel.Name;
+            profile.PhoneNumber = updateRequestModel.PhoneNumber;
+            _context.Entry(profile).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                responseModel.IsSuccess = true;
+            }
+            catch (Exception)
+            {
+                responseModel.IsSuccess = false;
+                return responseModel;
+            }
+
+            responseModel.Profile = profile;
+
+            
+
+            return responseModel;
+        }
+
+
+
+
+
+
     }
 }
---
+++ BookMyMealAPI/Model/Response/ProfileUpdateResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using BookMyMealAPI.Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookMyMealAPI.Model.Response
+{
+    public class ProfileUpdateResponseModel
+    {
+        public bool IsSuccess { get; set; }
+
+        public ProfileModel Profile { get; set; }
+    }
+}
